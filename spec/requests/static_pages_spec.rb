a5f4b655288ef538008c22b4054243f31d2143e5
Harry Edwards <harryedwards@mac.com>
2013-02-23 11:16:38 -0800

    finished contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) {  "Ruby on Rails Tutorial Sample App"}
   
   describe "Home page" do
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-      	:text => "Ruby on Rails Tutorial Sample App | Home") 
+      	:text => "#{base_title} | Home") 
     end
 
   end
@@ -25,7 +27,7 @@ describe "StaticPages" do
       it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-      	:text => "Ruby on Rails Tutorial Sample App | Help") 
+      	:text => "#{base_title} | Help") 
     end
   end
 
@@ -38,7 +40,20 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-      	:text => "Ruby on Rails Tutorial Sample App | About") 
+      	:text => "#{base_title} | About") 
+    end
+  end
+
+    describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+    	visit '/static_pages/contact'
+    	page.should have_content('Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+      	:text => "#{base_title} | Contact Us") 
     end
   end
 
